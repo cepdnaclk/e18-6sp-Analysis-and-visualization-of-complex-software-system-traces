26c7cab9c5d4fcbbd73f539024cdabcea3dd8463
IshanFernando <e18098@eng.pdn.ac.lk>
2023-06-13 08:01:09 +0530

    Add logo

--- frontend/src/components/AppSidebar.js
+++ frontend/src/components/AppSidebar.js
@@ -8,9 +8,10 @@ import { AppSidebarNav } from './AppSidebarNav'
 
 import { logoNegative } from 'src/assets/brand/logo-negative'
 import { sygnet } from 'src/assets/brand/sygnet'
-
+import { CImage } from '@coreui/react'
 import SimpleBar from 'simplebar-react'
 import 'simplebar/dist/simplebar.min.css'
+import ExactProImg from 'src/assets/images/exactpro-logo.png'
 
 // sidebar nav config
 import navigation from '../_nav'
@@ -30,7 +31,7 @@ const AppSidebar = () => {
       }}
     >
       <CSidebarBrand className="d-none d-md-flex" to="/">
-        <CIcon className="sidebar-brand-full" icon={logoNegative} height={35} />
+        <CImage rounded thumbnail src={ExactProImg} width={500} height={200} />
         <CIcon className="sidebar-brand-narrow" icon={sygnet} height={35} />
       </CSidebarBrand>
       <CSidebarNav>
