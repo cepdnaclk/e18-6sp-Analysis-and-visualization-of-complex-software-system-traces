a007464b6a01b4c23fa92f89e5696300a3b2a55c
IshanFernando <e18098@eng.pdn.ac.lk>
2023-09-14 20:22:21 +0530

    Add frontend code to send file to backend

--- frontend/src/views/Uploads/uploads.js
+++ frontend/src/views/Uploads/uploads.js
@@ -3,11 +3,35 @@ import { useDropzone } from 'react-dropzone';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFileUpload } from '@fortawesome/free-solid-svg-icons';
 import './FileUploadPage.css'; // Import your custom CSS for styling
+import axios from 'axios';
 
 const FileUploadPage = () => {
-  const onDrop = useCallback((acceptedFiles) => {
+  const onDrop = useCallback(async (acceptedFiles) => {
     // Handle the uploaded files here
     console.log('Accepted files:', acceptedFiles);
+
+    // Create a FormData object to append files
+    const formData = new FormData();
+
+    // Append each accepted file to the FormData object
+    acceptedFiles.forEach((file) => {
+      formData.append('file', file);
+    });
+
+    try {
+      // Make an HTTP POST request to your backend to upload the files
+      const response = await axios.post('http://localhost:5000/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data', // Set the content type to multipart/form-data
+        },
+      });
+
+      // Handle the response from the server
+      console.log('Server response:', response.data);
+    } catch (error) {
+      // Handle any errors that occur during the request
+      console.error('Error uploading files:', error);
+    }
   }, []);
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
