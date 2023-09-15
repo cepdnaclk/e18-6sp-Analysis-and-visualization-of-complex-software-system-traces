611ed252d733600287b4da62abf774811b705145
ridmajayasundara <e18155@eng.pdn.ac.lk>
2023-09-15 23:39:27 +0530

    added graph to dashboard

--- frontend/src/views/dashboard/Dashboard.js
+++ frontend/src/views/dashboard/Dashboard.js
@@ -1,6 +1,9 @@
 import React from 'react'
 // import data from './file_info.json'
 import data from '../../logStat/file_info'
+import './Dashboard.css' // Import a CSS file for custom styles
+import image4 from './image4.png';
+
 
 import {
   CChartBar,
@@ -64,66 +67,110 @@ import avatar6 from 'src/assets/images/avatars/6.jpg'
 import WidgetsBrand from '../widgets/WidgetsBrand'
 import WidgetsDropdown from '../widgets/WidgetsDropdown'
 
+const DescriptionComponent = () => {
+  return (
+    <div className="description">
+      <p>
+        This is the raw data used in Machine Learning-based Anomaly Detection in Software Systems:
+        An Industrial Evaluation.
+      </p>
+    </div>
+  )
+}
+const DescriptionComponent2 = () => {
+  return (
+    <div className="description">
+      <p>Exploratory Data Analysis </p>
+    </div>
+  )
+}
 const Dashboard = () => {
   const random = () => Math.round(Math.random() * 100)
 
   return (
     <>
       <CRow>
-        <center>
-      <CCol xs={8}>
-        <CCard className="mb-4">
-          <CCardHeader>Number of Data Frames in Each Cluster</CCardHeader>
-          <CCardBody>
-            <CChartBar
-              data={{
-                labels: ['Cluster 1', 'Cluster 2', 'Cluster 3', 'Cluster 4', 'Cluster 5', 'Cluster 6'],
-                datasets: [
-                  {
-                    label: 'Number of Data Frames',
-                    backgroundColor: '#008080',
-                    data: [28,10,12,13,15,9,12],
-                  },
-                ],
-              }}
-              labels="months"
-            />
-          </CCardBody>
-        </CCard>
-      </CCol>
-      </center>
-<CTable align="middle" className="mb-0 border" hover responsive>
-  <CTableHead color="light">
-    <CTableRow>
-      <CTableHeaderCell>File Name</CTableHeaderCell>
-      <CTableHeaderCell>Number of Columns</CTableHeaderCell>
-      <CTableHeaderCell>Size</CTableHeaderCell>
-      <CTableHeaderCell>Line Count</CTableHeaderCell>
-    </CTableRow>
-  </CTableHead>
-  <CTableBody>
-    {data.map((item, index) => (
-      <CTableRow
-        key={index}
-        style={{ backgroundColor: index % 2 === 0 ? '#f9f9f9' : 'white' }} // Alternate row colors
-      >
-        <CTableDataCell>
-          <div>{item.name}</div>
-        </CTableDataCell>
-        <CTableDataCell>
-          <div>{item.columns}</div>
-        </CTableDataCell>
-        <CTableDataCell>
-          <div>{item.size}</div>
-        </CTableDataCell>
-        <CTableDataCell>
-          <div>{item.line_count}</div>
-        </CTableDataCell>
-      </CTableRow>
-    ))}
-  </CTableBody>
-</CTable>
+        <CCol md={12}>
+          <div className="topic-heading-fancy">About Dataset</div>
 
+          <DescriptionComponent />
+        </CCol>
+      </CRow>
+      <center>
+        <h4>Clustering using Templated Based Approach </h4>
+      </center>
+      <CRow>
+        <center>
+          <CCol xs={8}>
+            <CCard className="mb-4">
+              <CCardHeader>Number of Data Frames in Each Cluster</CCardHeader>
+              <CCardBody>
+                <CChartBar
+                  data={{
+                    labels: [
+                      'Cluster 1',
+                      'Cluster 2',
+                      'Cluster 3',
+                      'Cluster 4',
+                      'Cluster 5',
+                      'Cluster 6',
+                    ],
+                    datasets: [
+                      {
+                        label: 'Number of Data Frames',
+                        backgroundColor: '#008080',
+                        data: [28, 10, 12, 13, 15, 9, 12],
+                      },
+                    ],
+                  }}
+                  labels="months"
+                />
+              </CCardBody>
+            </CCard>
+          </CCol>
+        </center>
+        <center>
+          {' '}
+          <h4> Exploratory Data Analysis </h4>
+        </center>
+
+        {/* <DescriptionComponent2 /> */}
+
+        <CTable align="middle" className="mb-0 border" hover responsive>
+          <CTableHead color="light">
+            <CTableRow>
+              <CTableHeaderCell>File Name</CTableHeaderCell>
+              <CTableHeaderCell>Number of Columns</CTableHeaderCell>
+              <CTableHeaderCell>Size</CTableHeaderCell>
+              <CTableHeaderCell>Line Count</CTableHeaderCell>
+            </CTableRow>
+          </CTableHead>
+          <CTableBody>
+            {data.map((item, index) => (
+              <CTableRow
+                key={index}
+                style={{ backgroundColor: index % 2 === 0 ? '#f9f9f9' : 'white' }} // Alternate row colors
+              >
+                <CTableDataCell>
+                  <div>{item.name}</div>
+                </CTableDataCell>
+                <CTableDataCell>
+                  <div>{item.columns}</div>
+                </CTableDataCell>
+                <CTableDataCell>
+                  <div>{item.size}</div>
+                </CTableDataCell>
+                <CTableDataCell>
+                  <div>{item.line_count}</div>
+                </CTableDataCell>
+              </CTableRow>
+            ))}
+          </CTableBody>
+        </CTable>
+        <center>
+        <h2>K-Means Clustering</h2>
+        <img src={image4} alt="Description of the image" className="img-small" />
+        </center>
       </CRow>
     </>
   )
